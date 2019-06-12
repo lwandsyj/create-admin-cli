aba6790738e8ea1a13fa787efd8d0af34e1a9294
Levi <lwaisyj@163.com>
2019-06-12 16:20:50 +0800

    '第一版完成模板的下载和依赖安装'

---
+++ bin/create-admin.js
@@ -0,0 +1,17 @@
+let program = require('commander');
+
+let config = require('../package.json');
+
+program
+    .version(config.version)
+    .usage('<command> [options]')
+
+
+program.command('init <projectName>','创建项目')
+    .alias('i')
+
+program.command('create <moduleName>','创建模块')
+    .alias('c')
+
+
+program.parse(process.argv);
